ec85df3a5234cb0bb7a4036ccf76757818dfe524
Laís Hillary <laishillary20@gmail.com>
2024-11-11 12:33:04 -0300

    Update Blogin.py

--- src/models/Blogin.py
+++ src/models/Blogin.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import pandas as pd
 
 file = Path(__file__).resolve()
 parent = file.parent
@@ -7,9 +8,18 @@ root =  file.parent.parent  # Importações relativas
 
 
 sys.path.append(str(root))
+def importar_credenciais(caminho_arquivo):
+    # Carregar o arquivo Excel
+    df = pd.read_excel(caminho_arquivo)
 
+    # Converter para dicionário onde o 'usuario' é a chave e 'senha' é o valor
+    user_credentials = dict(zip(df['Email'], df['Senha']))
+    
+    return user_credentials
 
-USER_CREDENTIALS = {"usuario": "senha123"}
+# Caminho do arquivo .xlsx
+caminho_arquivo = "Contas - PMGAS.xlsx"
+USER_CREDENTIALS = importar_credenciais(caminho_arquivo)
 
 
 def check_email(email): #Verifica se o email existe
