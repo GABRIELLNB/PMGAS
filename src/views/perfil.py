99eaee02826028725846c5a5ef252f85f5f4034a
Laís Hillary <laishillary20@gmail.com>
2024-11-11 22:42:51 -0300

    Create perfil.py

---
+++ src/views/perfil.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+def perfil(page: ft.Page):
+    perf = ft.Column(
+        controls=[
+            ft.Container(
+                alignment=ft.alignment.top_center,  # Alinha o container ao topo central
+                bgcolor=ft.colors.ORANGE,
+                border_radius=10,
+                padding=ft.padding.all(10),
+                width=1000,  # Largura do container
+                height=500,
+            )
+        ]
+    )
+    return perf
