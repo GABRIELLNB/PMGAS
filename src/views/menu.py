99abd58fb68c19f26567cc1770141c229a737ff3
Laís Hillary <laishillary20@gmail.com>
2024-11-11 22:43:06 -0300

    Update menu.py

--- src/views/menu.py
+++ src/views/menu.py
@@ -1,15 +1,33 @@
 import flet as ft
+import sys
+from pathlib import Path
+
+file = Path(__file__).resolve()
+parent = file.parent
+root =  file.parent.parent  # Importações relativas
+
+
+sys.path.append(str(root))
+
+from configuracoes import configuracoes
 
 def menu(page: ft.Page):
     # Define o título da página
     page.title = "PMGAS"
     
+    def update_content(content):
+        page.controls.clear()  # Limpa o conteúdo da página
+        page.controls.append(navigation_bar)  # Re-adiciona a barra de navegação
+        page.controls.append(content)  # Adiciona o novo conteúdo
+        page.update()  # Atualiza a página
+
+    
     # Cria a barra de navegação personalizada
     navigation_bar = ft.NavigationBar(
         destinations=[
-            ft.NavigationBarDestination(icon=ft.icons.HOME, label="Inicio"),
+            ft.NavigationBarDestination(icon=ft.icons.HOME, label="Inicio" ),
             ft.NavigationBarDestination(icon=ft.icons.DOCUMENT_SCANNER_SHARP, label="Cadastros"),
-            ft.NavigationBarDestination(icon=ft.icons.BAR_CHART, selected_icon=ft.icons.BOOKMARK, label="Gráficos"),
+            ft.NavigationBarDestination(icon=ft.icons.BAR_CHART, label="Gráficos"),
             ft.NavigationBarDestination(icon=ft.icons.PERSON_ROUNDED, label="Perfil"),
             ft.NavigationBarDestination(icon=ft.icons.SETTINGS, label="Configurações"),
         ]
