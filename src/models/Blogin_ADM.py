a5efe7991dd2b19245fc7f87b704c81860b93e3f
Laís Hillary <laishillary20@gmail.com>
2024-11-24 02:14:32 -0300

    Create Blogin_ADM.py

---
+++ src/models/Blogin_ADM.py
@@ -0,0 +1,29 @@
+import sys
+from pathlib import Path
+import pandas as pd
+
+file = Path(__file__).resolve()
+parent = file.parent
+root =  file.parent.parent  # Importações relativas
+
+
+sys.path.append(str(root))
+def importar_credenciais(caminho_arquivo):
+    # Carregar o arquivo Excel
+    df = pd.read_excel(caminho_arquivo)
+
+    # Converter para dicionário onde o 'usuario' é a chave e 'senha' é o valor
+    user_credentials = dict(zip(df['Email'], df['Senha']))
+    
+    return user_credentials
+
+# Caminho do arquivo .xlsx
+caminho_arquivo = "Contas - PMGAS - ADM.xlsx"
+USER_CREDENTIALS = importar_credenciais(caminho_arquivo)
+
+
+def check_email(email): #Verifica se o email existe
+    return email in USER_CREDENTIALS
+
+def check_senha(email, senha): #Verifica se a senha corresponde ao email informado
+    return USER_CREDENTIALS.get(email) == senha
