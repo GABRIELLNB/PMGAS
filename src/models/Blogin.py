9570eae021d968193f009658489990a2d6201f4c
LsyralliH <133071553+LsyralliH@users.noreply.github.com>
2024-11-30 12:41:22 -0300

    Update Blogin.py

--- src/models/Blogin.py
+++ src/models/Blogin.py
@@ -8,8 +8,9 @@ root =  file.parent.parent  # Importações relativas
 
 #BAIXAR pip install openpyxl e pip install pandas
 
-
 sys.path.append(str(root))
+
+
 def importar_credenciais(caminho_arquivo):
     # Carregar o arquivo Excel
     df = pd.read_excel(caminho_arquivo)
@@ -23,9 +24,8 @@ def importar_credenciais(caminho_arquivo):
 caminho_arquivo = "Contas - PMGAS.xlsx"
 USER_CREDENTIALS = importar_credenciais(caminho_arquivo)
 
-
-def check_email(email): #Verifica se o email existe
+def check_email(email):  # Verifica se o email existe
     return email in USER_CREDENTIALS
 
-def check_senha(email, senha): #Verifica se a senha corresponde ao email informado
+def check_senha(email, senha):  # Verifica se a senha corresponde ao email informado
     return USER_CREDENTIALS.get(email) == senha
