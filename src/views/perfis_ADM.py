b7dd063d6693f8f39303854a0e8e0d926a0857a5
LsyralliH <133071553+LsyralliH@users.noreply.github.com>
2024-12-02 19:32:56 -0300

    Update perfis_ADM.py

--- src/views/perfis_ADM.py
+++ src/views/perfis_ADM.py
@@ -201,7 +201,7 @@ def perfil_adm(page: ft.Page):
             ),
                 navigation_bar(update_content, configuracoes_content, cadastros_content, perfil_content),
                 ft.Row(
-                    alignment=ft.MainAxisAlignment.END,  # Alinha os itens à direita
+                    alignment=ft.MainAxisAlignment.CENTER,  # Alinha os itens à direita
                     vertical_alignment=ft.CrossAxisAlignment.CENTER,  # Alinha verticalmente ao centro
                     controls=[
                         # SearchBar
@@ -228,22 +228,8 @@ def perfil_adm(page: ft.Page):
                             ),
                             width=1000,  # Ajusta a largura do SearchBar
                         ),
-                        ft.Container(width=150),
-                        # Botão com ícone
-                        ft.Container(
-                            alignment=ft.alignment.center,
-                            bgcolor=a2,
-                            width=40,  # Largura do círculo
-                            height=40,  # Altura do círculo
-                            content=ft.Icon(
-                                ft.icons.EDIT_SQUARE,  # Ícone de notificação
-                                size=40,  # Tamanho do ícone ajustado para maior visibilidade
-                                color=a1  # Cor do ícone
-                            ),
-                        ),
-                        
-                        ft.Container(width=50),
-                        
+
+            
                     ],
                 ),
                 ft.Divider(
